70be3ec2c885a74ccdc2af7edf56e96744ebe266
hyuna919 <hyuna919@naver.com>
2025-03-18 14:50:39 +0900

    fix: useEffect 무한 패치 문제 해결

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import { fetchCigarettes } from "./firebase";
 function App() {
   const [cigarettes, setCigarettes] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [showBrand, setShowBrand] = useState(false);
+  const [fullname, setFullname] = useState(false);
 
   useEffect(() => {
-    fetchCigarettes().then((data) => setCigarettes(data));
+    fetchCigarettes().then((data) => {
+      data = data.map((item) => {
+        const question = item.nickname.filter((el) => el && String(el).trim());
+        question.push(item.short_name);
+
+        return {
+          ...item,
+          question,
+          qLength: question.length, // 여기서 바로 question.length 사용
+        };
+      });
+
+      setCigarettes(data);
+    }, []);
   }, []);
 
   const handleNext = () => {
-    if (!showBrand) {
-      setShowBrand(true);
+    if (!fullname) {
+      setFullname(true);
     } else {
-      setShowBrand(false);
+      setFullname(false);
       setCurrentIndex((prevIndex) => (prevIndex + 1) % cigarettes.length);
     }
   };
@@ -32,15 +45,23 @@ function App() {
       <div className="text-center mb-6">
         <h1 className="text-3xl font-bold">담배 퀴즈</h1>
         <p className="text-lg text-gray-200">
-          이름을 보고 브랜드를 맞혀보세요!
+          담배의 이름과 별명을 보고 매대에서 담배를 찾으세요!
         </p>
       </div>
       <div
         className="p-6 bg-white text-gray-900 rounded-2xl shadow-lg text-center w-full max-w-xs cursor-pointer transition-transform transform active:scale-95"
         onClick={handleNext}
       >
-        <h1 className="text-2xl font-bold">{cigarettes[currentIndex].name}</h1>
-        {showBrand && (
+        {/* 랜덤으로 하나 뽑아서 질문 */}
+        <h1 className="text-2xl font-bold">
+          {
+            /* {cigarettes[currentIndex].name + */
+            cigarettes[currentIndex].question[
+              Math.floor(Math.random() * cigarettes[currentIndex].qLength)
+            ]
+          }
+        </h1>
+        {fullname && (
           <h2 className="text-xl text-gray-600 mt-2">
             {cigarettes[currentIndex].full_name}
           </h2>
